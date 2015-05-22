844e85ac0623b84f6ec3219b148794686d6b9cc9
Andrew <awolfe76@gmail.com>
2015-05-22 10:17:50 -0400

    change text on show/hide details

--- js/app.js
+++ js/app.js
@@ -5,9 +5,11 @@ $(function() {
         if ($(this).parent('td').parent('tr').hasClass('no-border')) {
             $(this).parent('td').parent('tr').removeClass('no-border');
             $(this).parent('td').parent('tr').next().addClass('hide');
+            $(this).text('Show details');
         } else {
             $(this).parent('td').parent('tr').addClass('no-border');
             $(this).parent('td').parent('tr').next().removeClass('hide');
+            $(this).text('Hide details');
         }
         return false;
     });
