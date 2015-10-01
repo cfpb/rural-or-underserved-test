f0612d15fc85ba901fc32101ec9a3c9c404f521e
Andrew <awolfe76@gmail.com>
2015-10-01 14:30:43 -0400

    better associate map with row; some cleanup

--- src/js/showMap.js
+++ src/js/showMap.js
@@ -2,59 +2,48 @@ require('mapbox.js');
 
 L.mapbox.accessToken = 'pk.eyJ1IjoiY29tcHV0ZWNoIiwiYSI6InMyblMya3cifQ.P8yppesHki5qMyxTc2CNLg';
 
+// when a.jsLoadMap is clicked
 $('body').on('click', 'a.jsLoadMap', function(e) {
   e.preventDefault();
 
-  /*var id = $(this).data('id');
-
-  // check if id already exists
-  if ($('#loc-' + id).length) {
-    // remove the map and container
-    //$('#loc-' + id).remove();
-    $(this).parents('tbody > tr').eq(rowIndex).remove();
-  } else {
-    // get the lat/lon
-    var lat = $(this).data('lat');
-    var lon = $(this).data('lon');
-    
-    //console.log($(this).parents('tr').index());
-    var rowIndex = $(this).parents('tr').index();
-    console.log(rowIndex);
-    $(this).parents('tbody > tr').eq(rowIndex - 1).after('<tr><td colspan="5"><div class="map" id="loc-' + id + '"></div></td></tr>');
-    // append the map container
-    //$(this).parent().append('<div class="map" id="loc-' + id + '"></div>');
-
-    // create the map and add marker
-    var latlng = L.latLng(lon, lat);
-    map = L.mapbox.map('loc-' + id, 'cfpb.k55b27gd', { center: latlng }).setView(latlng,15);
-    var marker = L.marker(latlng).addTo(map);
-  }*/
-
+  // setup vars (data attributes)
   var lat = $(this).data('lat');
   var lon = $(this).data('lon');
   var id = $(this).data('id');
-  //var mapShown = $(this).data('map')
+  var mapShown = $(this).data('map');
 
+  // if the map row is hidden
   if ($(this).parents('tr').next().hasClass('hide')) {
+    // show it
     $(this).parents('tr').next().removeClass('hide');
+    // remove border to better associate map with row
+    $(this).parents('tr').children('td').addClass('no-border');
+    // change text
     $(this).text('Hide map');
     
-    if ($(this).data('map') === false) {
+    // only show initiate the map the first time
+    if (mapShown === false) {
+      // set the map to true (won't try to initate again)
       $(this).data('map', 'true');
+
+      // setup and app map
       var latlng = L.latLng(lon, lat);
-
       map = L.mapbox.map(id, 'cfpb.k55b27gd', { center: latlng, zoomControl: false }).setView(latlng,10);
-
       map.dragging.disable();
       map.touchZoom.disable();
       map.doubleClickZoom.disable();
       map.scrollWheelZoom.disable();
       if (map.tap) map.tap.disable();
       
+      // add marker
       var marker = L.marker(latlng).addTo(map);
     }
-  } else {
+  } else {  // map is being displayed
+    // hide it
     $(this).parents('tr').next().addClass('hide');
+    // add border back
+    $(this).parents('tr').children('td').removeClass('no-border');
+    // change text
     $(this).text('Show map');
   }
 });
