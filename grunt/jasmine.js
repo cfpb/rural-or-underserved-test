341e2804b4a7b7ca8b1d1b3189e2f5681bec4599
Andrew <awolfe76@gmail.com>
2015-11-16 12:20:53 -0500

    jasmine and coverage setup, watched; isFound tests

--- grunt/jasmine.js
+++ grunt/jasmine.js
@@ -4,5 +4,17 @@ module.exports = {
     options : {
       specs : 'test/specs.js'
     }
+  },
+  coverage: {
+    src : 'dist/js/cricket.js',
+    options : {
+      specs : 'test/specs.js',
+
+      template: require('grunt-template-jasmine-istanbul'),
+      templateOptions: {
+        coverage: 'coverage/coverage.json',
+        report: 'coverage'
+      }
+    }
   }
 }
