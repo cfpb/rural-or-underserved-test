cdd44bd5183858e21d60ab30a7b425e788ca529d
Andrew <awolfe76@gmail.com>
2015-12-08 14:22:55 -0500

    add radix to parseInt

--- src/js/count.js
+++ src/js/count.js
@@ -16,7 +16,7 @@ module.exports = function() {
       var verb = 'are';
 
       // add one to correct type
-      var typeCount = parseInt($('a.' + type + 'Cnt').text());
+      var typeCount = parseInt($('a.' + type + 'Cnt').text(), 10);
       typeCount++;
       $('.' + type + 'Cnt').text(typeCount);
 
@@ -30,7 +30,7 @@ module.exports = function() {
       $('.' + type + 'Case').text(noun + ' ' + verb);
 
       // add one to the total
-      var totalCount = parseInt($('#totalCnt').text());
+      var totalCount = parseInt($('#totalCnt').text(), 10);
       totalCount++;
       $('#totalCnt').text(totalCount);
     }
