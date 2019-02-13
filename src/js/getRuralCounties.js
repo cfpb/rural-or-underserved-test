9d5a6334f2b86fffa22a0b01c4aba80b4f0f3aed
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2019-02-12 17:57:00 -0800

    include path

--- src/js/getRuralCounties.js
+++ src/js/getRuralCounties.js
@@ -2,5 +2,5 @@ require( 'es6-promise' ).polyfill();
 var axios = require( 'axios' );
 
 module.exports = function( year ) {
-  return axios.get( window.location.origin + '/data/' + year + '.json' );
+  return axios.get( window.location.origin + window.location.pathname + 'data/' + year + '.json' );
 };
